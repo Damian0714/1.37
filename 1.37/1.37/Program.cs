97050a7d42eaf6d7a374fa7c47039299f8d1c8d0
Damian0714 <113889129+Damian0714@users.noreply.github.com>
2023-02-13 14:15:54 +0300

    Add files via upload

---
+++ 1.37/1.37/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите первый и второй катеты");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = Math.Sqrt((a * a) + (b * b));
+Console.WriteLine("Периметр треугольника равен: {0}", c);
